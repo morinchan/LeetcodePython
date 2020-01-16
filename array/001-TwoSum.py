607c047842ffd250e8b339be3e230c17404b2476
morinchan <momo_jy@163.com>
2020-01-16 19:51:31 +0800

    Update and rename 01.TwoSum to 001-TwoSum.py

--- array/001-TwoSum.py
+++ array/001-TwoSum.py
@@ -1,4 +1,4 @@
-
+```
 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -30,6 +30,8 @@ class Solution:
 
 存在问题:直接循环存在重复计算。
 新方法：下标可以使用dict的key和value，用enumerate迭代。
+```
+
 
 class Solution:
     def twoSum(self, nums, target):
